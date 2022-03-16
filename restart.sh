1ec444b3762ca496e6da8a17afae39f753d97cbd
Daenges <Daenges2@gmail.com>
2022-03-16 13:26:44 +0100

    Fix indentation

--- restart.sh
+++ restart.sh
@@ -49,16 +49,16 @@ while [ true ]; do
 
     if [[ ! -d "exit_codes" ]]; then
             mkdir "exit_codes";
-        fi
-        if [[ ! -f "exit_codes/server_exit_codes.log" ]]; then
-            touch "exit_codes/server_exit_codes.log";
-        fi
-        echo "[$(date +"%d.%m.%Y %T")] ExitCode: $?" >> exit_codes/server_exit_codes.log
-        echo "----- Press enter to prevent the server from restarting in $RestartDelaySeconds seconds -----";
-        read -t $RestartDelaySeconds input;
-        if [ $? == 0 ]; then
-            break;
-        else
-            echo "------------------- SERVER RESTARTS -------------------";
-        fi
+    fi
+    if [[ ! -f "exit_codes/server_exit_codes.log" ]]; then
+        touch "exit_codes/server_exit_codes.log";
+    fi
+    echo "[$(date +"%d.%m.%Y %T")] ExitCode: $?" >> exit_codes/server_exit_codes.log
+    echo "----- Press enter to prevent the server from restarting in $RestartDelaySeconds seconds -----";
+    read -t $RestartDelaySeconds input;
+    if [ $? == 0 ]; then
+        break;
+    else
+        echo "------------------- SERVER RESTARTS -------------------";
+    fi
 done
